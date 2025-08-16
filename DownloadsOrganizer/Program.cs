320b0bc08ae8c9c03d19f1ab374cdb7ab7320a94
Davis Jhonns <davis.jhonns@gmail.com>
2025-08-16 20:59:23 +0200

    everything

---
+++ DownloadsOrganizer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DownloadsOrganizer
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var view = new MainForm();
+            var service = new FileOrganizerService();
+            var presenter = new OrganizerPresenter(view, service);
+            Application.Run(view);
+        }
+    }
+}
